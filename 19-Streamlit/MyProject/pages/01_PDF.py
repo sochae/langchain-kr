48150b498b8c20fc11d1f8380e659bef376e892b
LAPTOP-TECHQ7AK\bleug <bleugirl@naver.com>
2025-02-06 17:04:57 +0900

    PDF기반 QA 챗봇 만들기

--- 19-Streamlit/MyProject/pages/01_PDF.py
+++ 19-Streamlit/MyProject/pages/01_PDF.py
@@ -4,9 +4,17 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_teddynote.prompts import load_prompt
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyMuPDFLoader, PDFPlumberLoader
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from dotenv import load_dotenv
 import glob
 import os
+
 
 # API KEY 정보로드
 load_dotenv()
@@ -31,6 +39,10 @@ st.title("PDF 기반 QA🚨")
 if "messages" not in st.session_state:
     # 대화기록을 저장하기 위한 용도로 생성한다.
     st.session_state["messages"] = []
+    
+if "chain" not in st.session_state:
+  st.session_state["chain"] = None
+
 
 # 사이드바 생성
 with st.sidebar:
@@ -60,27 +72,70 @@ def embed_file(file):
   with open(file_path, "wb") as f:
     f.write(file_content)
 
-# 파일이 업로드 되었을 때
-if uploaded_file: 
-  embed_file(uploaded_file)
+    # 단계 1: 문서 로드(Load Documents)
+    loader = PyMuPDFLoader(file_path)
+    docs = loader.load()
 
+    # 단계 2: 문서 분할(Split Documents)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+    split_documents = text_splitter.split_documents(docs)
 
+    # 단계 3: 임베딩(Embedding) 생성
+    embeddings = OpenAIEmbeddings()
+
+    # 단계 4: DB 생성(Create DB) 및 저장
+    # 벡터스토어를 생성합니다.
+    vectorstore = FAISS.from_documents(documents=split_documents, embedding=embeddings)
+
+    # 단계 5: 검색기(Retriever) 생성
+    # 문서에 포함되어 있는 정보를 검색하고 생성합니다.
+    retriever = vectorstore.as_retriever()
+    return retriever
+    
 # 체인 생성
-def create_chain(prompt_filepath):
+def create_chain(retriever):
     # prompt | llm | output_parser
     
     # prompt 적용
-    prompt = load_prompt(prompt_filepath, encoding="utf-8")
+    # prompt = load_prompt(prompt_filepath, encoding="utf-8")
     
-    # GPT
+    # 단계 6: 프롬프트 생성(Create Prompt)
+    # 프롬프트를 생성합니다.
+    prompt = PromptTemplate.from_template(
+        """You are an assistant for question-answering tasks. 
+    Use the following pieces of retrieved context to answer the question. 
+    If you don't know the answer, just say that you don't know. 
+    Answer in Korean.
+
+    #Context: 
+    {context}
+
+    #Question:
+    {question}
+
+    #Answer:"""
+    )
+
+    # 단계 7: 언어모델(LLM) 생성
+    # 모델(LLM) 을 생성합니다.
     llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
 
-    # 출력 파서
-    output_parser = StrOutputParser()
-
-    # 체인 생성
-    chain = prompt | llm | output_parser
+    # 단계 8: 체인(Chain) 생성
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
     return chain
+
+
+# 파일이 업로드 되었을 때
+if uploaded_file:
+  # 파일 업로드 후 retriever 생성 (작업시간이 오래 걸릴 예정...) 
+  retriever = embed_file(uploaded_file)
+  chain = create_chain(retriever)
+  st.session_state["chain"] = chain
 
 
 # 초기화 버튼이 눌리면...
@@ -93,24 +148,33 @@ print_messages()
 # 사용자의 입력
 user_input = st.chat_input("궁금한 내용을 물어보세요!")
 
+# 경고 메시지를 띄우기 위한 빈 영역
+warning_msg = st.empty()
+
 # 만약에 사용자 입력이 들어오면...
 if user_input:
-    # 사용자의 입력
-    st.chat_message("user").write(user_input)
     # chain 을 생성
-    chain = create_chain(selected_prompt)
+    chain = st.session_state["chain"]
+    
+    if chain is not None:
+      # 사용자의 입력
+      st.chat_message("user").write(user_input)
+      
+      # 스트리밍 호출
+      response = chain.stream(user_input)
+      with st.chat_message("assistant"):
+          # 빈 공간(컨테이너)을 만들어서, 여기에 토큰을 스트리밍 출력한다.
+          container = st.empty()
 
-    # 스트리밍 호출
-    response = chain.stream({"question": user_input})
-    with st.chat_message("assistant"):
-        # 빈 공간(컨테이너)을 만들어서, 여기에 토큰을 스트리밍 출력한다.
-        container = st.empty()
+          ai_answer = ""
+          for token in response:
+              ai_answer += token
+              container.markdown(ai_answer)
 
-        ai_answer = ""
-        for token in response:
-            ai_answer += token
-            container.markdown(ai_answer)
-
-    # 대화기록을 저장한다.
-    add_message("user", user_input)
-    add_message("assistant", ai_answer)
+      # 대화기록을 저장한다.
+      add_message("user", user_input)
+      add_message("assistant", ai_answer)
+      
+    else:
+      # 파일을 업로드 하라는 경로 메시지 출력
+      warning_msg.error("파일을 업로드 해주세요.")
